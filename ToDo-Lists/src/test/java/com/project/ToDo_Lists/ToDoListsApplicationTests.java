8dc791466f7c71bd59ca23e211d27cbf9d5fc712
Subhojit Roy <roysubhojit870@gmail.com>
2026-09-20 20:07:17 +0530

    Add files via upload

---
+++ ToDo-Lists/src/test/java/com/project/ToDo_Lists/ToDoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ToDo_Lists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ToDo_Lists/ToDoListsApplication.java
@@ -0,0 +1,13 @@
+package com.project.ToDo_Lists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListsApplication.class, args);
+	}
+
+}
